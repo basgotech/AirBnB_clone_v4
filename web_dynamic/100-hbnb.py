4267f236fe4d9b4b0f25cc21d38c2b3e623448ea
Basliel Tegegn <basgolilo5@gmail.com>
2024-05-04 00:06:27 -0700

    adding

---
+++ web_dynamic/100-hbnb.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""
+Flask App reproduce the same steps with the State and City filter
+"""
+from flask import Flask, render_template, url_for
+from models import storage
+import uuid;
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+port = 5000
+host = '0.0.0.0'
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """
+    asession close after finish
+    """
+    storage.close()
+
+
+@app.route('/100-hbnb')
+def hbnb_filters_on(the_id=None):
+    """
+    handles request of 100-hbnb.html files
+    """
+    state_objs = storage.all('State').values()
+    states = dict([state.name, state] for state in state_objs)
+    amens = storage.all('Amenity').values()
+    places = storage.all('Place').values()
+    users = dict([user.id, "{} {}".format(user.first_name, user.last_name)]
+                 for user in storage.all('User').values())
+    return render_template('100-hbnb.html',
+                           cache_id=uuid.uuid4(),
+                           states=state_objs,
+                           amens=amens,
+                           places=places,
+                           users=users)
+
+if __name__ == "__main__":
+    """
+    MAIN Functions"""
+    app.run(host=host, port=port)
